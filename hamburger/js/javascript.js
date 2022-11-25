3a60b8d7f67b75622094e3e1e1f4055c0c450391
adrienne_b <WorldofArts_95@yahoo.com>
2022-11-24 18:37:10 -0600

    Have mobile, tablet, and desktop. Working on navigation/hamburger, footer, red wine section, special effects.

---
+++ hamburger/js/javascript.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+
+function onClickMenu() {
+	
+	document.getElementById("menu").classList.toggle("change");
+	
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+var button = document.querySelector(" #menu-bar");
+var modal = document.querySelector("nav");
+
+
+nav.addEventListener("click", function () {
+  modal.classList.add(".menu-bar");
+
+});
+
+modalX.addEventListener("click", function () {
+  modal.classList.remove("nav");
+});
+
+
